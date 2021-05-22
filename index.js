731afeeef8dacdf80d0221bf6ba48538cb327184
RP <rahulpahuja2015@gmail.com>
2021-05-22 12:37:01 +0530

    Removed 2 Servers

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+var app = express();
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
+// //.get('/', (req, res) => res.render('pages/index'))
 
-var app = express();
 
 
 app.get('/hello',function(req,res){
