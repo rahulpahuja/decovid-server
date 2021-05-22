ddfb60fa455755e7aca57e8d9efa0ce616dbf96f
RP <rahulpahuja2015@gmail.com>
2021-05-22 12:33:54 +0530

    Added 2nd path

--- index.js
+++ index.js
@@ -13,64 +13,64 @@ express()
 var app = express();
 
 
-  app.get('/hello',function(req,res){
+app.get('/hello',function(req,res){
   res.send('Hello World')
 });
 
-// express.get('/suppliers',function(req,res){
-//  var object = {
-//   "isActive": "false",
-//   "name": "Rahul",
-//   "phoneNumber": "8888888888",
-//   "supplies": "{BLOOD,OXYGEN}",
-//   "id": "1",
-//   "state": "Madhya Pradesh",
-//   "city": "Indore",
-//   "address": "Raja Ram Nagar",
-//   "organization": "Individual",
-//   "isReported": "false",
-//   "reportedCount": "0",
-//   "image": "www.google.com"
-// }
-// var object1 = {
-//   "isActive": "false",
-//   "name": "Raj",
-//   "phoneNumber": "8888888888",
-//   "supplies": "{BLOOD,OXYGEN}",
-//   "id": "2",
-//   "state": "Madhya Pradesh",
-//   "city": "Indore",
-//   "address": "Raja Ram Nagar",
-//   "organization": "Individual",
-//   "isReported": "false",
-//   "reportedCount": "0",
-//   "image": "www.google.com"
-// }
-// var object2 = {
-//   "isActive": "false",
-//   "name": "Rock",
-//   "phoneNumber": "8888888880",
-//   "supplies": "{BLOOD,OXYGEN}",
-//   "id": "3",
-//   "state": "Madhya Pradesh",
-//   "city": "Indore",
-//   "address": "Raja Ram Nagar",
-//   "organization": "Individual",
-//   "isReported": "false",
-//   "reportedCount": "0",
-//   "image": "www.google.com"
-// }
+app.get('/suppliers',function(req,res){
+ var object = {
+  "isActive": "false",
+  "name": "Rahul",
+  "phoneNumber": "8888888888",
+  "supplies": "{BLOOD,OXYGEN}",
+  "id": "1",
+  "state": "Madhya Pradesh",
+  "city": "Indore",
+  "address": "Raja Ram Nagar",
+  "organization": "Individual",
+  "isReported": "false",
+  "reportedCount": "0",
+  "image": "www.google.com"
+}
+var object1 = {
+  "isActive": "false",
+  "name": "Raj",
+  "phoneNumber": "8888888888",
+  "supplies": "{BLOOD,OXYGEN}",
+  "id": "2",
+  "state": "Madhya Pradesh",
+  "city": "Indore",
+  "address": "Raja Ram Nagar",
+  "organization": "Individual",
+  "isReported": "false",
+  "reportedCount": "0",
+  "image": "www.google.com"
+}
+var object2 = {
+  "isActive": "false",
+  "name": "Rock",
+  "phoneNumber": "8888888880",
+  "supplies": "{BLOOD,OXYGEN}",
+  "id": "3",
+  "state": "Madhya Pradesh",
+  "city": "Indore",
+  "address": "Raja Ram Nagar",
+  "organization": "Individual",
+  "isReported": "false",
+  "reportedCount": "0",
+  "image": "www.google.com"
+}
 
-//   var myList = new Array();
-//   myList.push(object1);
-//   myList.push(object);
-//   myList.push(object2);
-//   myList.push(object);
-//   myList.push(object);
-//   myList.push(object1);
-//   myList.push(object);
-//   myList.push(object2);
-//   myList.push(object2);
-//   myList.push(object2);
-//   res.send(myList)
-// });
+  var myList = new Array();
+  myList.push(object1);
+  myList.push(object);
+  myList.push(object2);
+  myList.push(object);
+  myList.push(object);
+  myList.push(object1);
+  myList.push(object);
+  myList.push(object2);
+  myList.push(object2);
+  myList.push(object2);
+  res.send(myList)
+});
