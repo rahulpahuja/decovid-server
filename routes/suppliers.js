64a1378da84d9cec1b8de2e575b7b339bb743d36
gouravjat <gourav.jat@live.com>
2021-05-22 14:50:11 +0530

    Added Supplier Route and server configuration.

---
+++ routes/suppliers.js
@@ -0,0 +1,140 @@
+const express = require("express");
+
+const supplierRouter = express.Router({ mergeParams: true });
+
+const Supplier = require("../model/Supplier");
+
+
+/** DUMMY GET ALL Suppliers **/
+supplierRouter.get("/", async (req, res) => {
+  var object = {
+    isActive: "false",
+    name: "Rahul",
+    phoneNumber: "8888888888",
+    supplies: "{BLOOD,OXYGEN}",
+    id: "1",
+    state: "Madhya Pradesh",
+    city: "Indore",
+    address: "Raja Ram Nagar",
+    organization: "Individual",
+    isReported: "false",
+    reportedCount: "0",
+    image: "www.google.com",
+  };
+  var object1 = {
+    isActive: "false",
+    name: "Raj",
+    phoneNumber: "8888888888",
+    supplies: "{BLOOD,OXYGEN}",
+    id: "2",
+    state: "Madhya Pradesh",
+    city: "Indore",
+    address: "Raja Ram Nagar",
+    organization: "Individual",
+    isReported: "false",
+    reportedCount: "0",
+    image: "www.google.com",
+  };
+  var object2 = {
+    isActive: "false",
+    name: "Rock",
+    phoneNumber: "8888888880",
+    supplies: "{BLOOD,OXYGEN}",
+    id: "3",
+    state: "Madhya Pradesh",
+    city: "Indore",
+    address: "Raja Ram Nagar",
+    organization: "Individual",
+    isReported: "false",
+    reportedCount: "0",
+    image: "www.google.com",
+  };
+
+  var myList = new Array();
+  myList.push(object1);
+  myList.push(object);
+  myList.push(object2);
+  myList.push(object);
+  myList.push(object);
+  myList.push(object1);
+  myList.push(object);
+  myList.push(object2);
+  myList.push(object2);
+  myList.push(object2);
+  res.send(myList);
+});
+
+/** Get all Suppliers */
+supplierRouter.get("/getAllSuppliers", async (req, res) => {
+  let suppliers = await Supplier.find({}).exec();
+  console.log(suppliers);
+  let responseObject = {
+    status: "success",
+    title: "Get all suppliers.",
+    msg: "Suppliers found.",
+    data: { suppliers: suppliers },
+  };
+
+  return res.status(200).json(responseObject);
+});
+
+
+/** Add a Supplier*/
+supplierRouter.post("/addSupplier", async (req, res) => {
+  console.log(req.body);
+  const { email, password } = req.body;
+
+  //check for existing supplier
+  try {
+    let existingSupplier = await Supplier.findOne({ email }).exec();
+    if (existingSupplier) {
+      res.status(400).json({
+        status: "error",
+        title: "CreateSupplierFailed",
+        msg: "Supplier already exists.",
+      });
+      return;
+    }
+
+    //create new Supplier
+    let supplier = new Supplier({ email, password });
+    await supplier.save();
+
+    let createdSupplier = await Supplier.findOne({ email }).exec();
+
+    res.status(200).json({
+      status: "success",
+      title: "CreateSupplier",
+      msg: "Supplier successfully created",
+      data: { supplier: createdSupplier },
+    });
+  } catch (error) {
+    res
+      .status(400)
+      .json({ status: "error", title: "CreateSuplierFailed", msg: error });
+  }
+});
+
+/** Get a supplier. */
+supplierRouter.get("/:email", async (req, res) => {
+  const { email } = req.params;
+
+  let existingSupplier = await Supplier.findOne({ email }).exec();
+  if (existingSupplier) {
+    res.status(200).json({
+      status: "success",
+      title: "Supplier found.",
+      msg: "Supplier successfully found",
+      data: { supplier: existingSupplier },
+    });
+    return;
+  } else {
+    res.status(400).json({
+      status: "error",
+      title: "CreateSupplierFailed",
+      msg: "Supplier already exists.",
+    });
+  }
+});
+
+module.exports = supplierRouter;
