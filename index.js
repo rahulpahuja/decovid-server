1f16f5e2b76b38de9b4da66924f75c644f7e817c
RP <rahulpahuja2015@gmail.com>
2021-05-22 12:40:58 +0530

    Added Hello World

--- index.js
+++ index.js
@@ -6,9 +6,10 @@ var app = express();
 app.use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
+  .get('/', (req, res) => res.send('Hello World'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
-// //.get('/', (req, res) => res.render('pages/index'))
+
 
 
 
