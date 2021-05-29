39b89f76819178c37ff9e6c76d2f0405ec5660d6
RP <rahulpahuja2015@gmail.com>
2021-05-29 12:33:11 +0530

    Added Supplier Agreement Schema

---
+++ model/SupplierAgreement.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const SupplierAgreementSchema = mongoose.Schema({
+  id: {
+    type: String,
+    required: true,
+  },
+  detail: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  isActive: {
+    type: Boolean,
+  }
+});
+
+// export model supplier with SupplierSchema
+module.exports = mongoose.model("Supplier", SupplierSchema);
