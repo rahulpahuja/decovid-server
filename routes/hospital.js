22878f10d5c7ed06e97ed57dce3665f3fc42407f
Rahul Pahuja <rahulpahuja2015@gmail.com>
2021-12-02 11:59:06 +0530

    Cleaned Hospital Model

--- routes/hospital.js
+++ routes/hospital.js
@@ -1,11 +1,9 @@
 const express = require("express");
-
 const hospitalRouter = express.Router({ mergeParams: true });
-
 const Hospital = require("../model/Hospital");
 
+//GET:Fetches all the Hospitals
 hospitalRouter.get("/",async(req,res)=>{
-
   try {
     let h = await Hospital.findAll().exec();
     res.status(200).json({
@@ -20,24 +18,15 @@ hospitalRouter.get("/",async(req,res)=>{
     .json({ status: "error", title: "No Hospital Exist, Try Again Later", msg: "Error" });
 }
 });
+//POST: Create New Hospital
 hospitalRouter.post("/addHospital", async (req, res) => {
     console.log(req.body);
     const { id } = req.body;
     //check for existing supplier
     try {
-    //  let v = await Voulenteer.findOne({ email }).exec();
-    //   if (v) {
-    //     res.status(400).json({
-    //       status: "error",
-    //       title: "Create Voulenteer Failed",
-    //       msg: "Voulenteer already exists.",
-    //     });
-    //     return;
-    //   }
       //create new Hospital
       let hp = new Hospital(req.body);
       await hp.save();
-
       let hp1 = await Hospital.findOne({ id }).exec();
       res.status(200).json({
         status: "success",
@@ -51,6 +40,4 @@ hospitalRouter.post("/addHospital", async (req, res) => {
         .json({ status: "error", title: "Create Hospital Failed, Try Again", msg: error });
     }
   });
-
-
 module.exports = hospitalRouter;
